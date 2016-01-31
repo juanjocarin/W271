0f6be977802f7752d88056223d261fd3dad26acd
songminghu2004 <songminghu2004@yahoo.com>
2016-01-30 22:43:40 -0500

    update Question 5,6,7
    update Question 5,6,7
    
    some sub-questions are still missing
    somehow the comment lines ,"# QUESTION ..." are printed right after the
    R code block.

--- HW1/code/W271_HW1_Carin-Davis-Levato-Song.R
+++ HW1/code/W271_HW1_Carin-Davis-Levato-Song.R
@@ -6,6 +6,7 @@
 ## @knitr Libraries
 #### LOAD LIBRARIES AND DEFINE CONSTANTS USED IN MULTIPLE CHUNKS
 library(knitr)
+library(car)
 
 # QUESTION 1 --------------------------------------------------------------
 # load the data
@@ -15,12 +16,16 @@ setwd("C:/Users/songminghu/UCB_DataScience/W271_ApplyRegressionTimeSeriesAnalysi
 load("birthweight_w271.Rdata")
 
 
+
+
 # QUESTION 2 --------------------------------------------------------------
 # check how many variables and observations in the data -------------------
 # there are  14 variables and 1388   observations in the data -------------
 ## @knitr Question2
 dim(desc)[1] # check number of variables, or use str(data) command
 dim(data)[1] # check number of observations, or use str(data) command
+
+
 
 
 # QUESTION 3 --------------------------------------------------------------
@@ -45,6 +50,8 @@ hist(data$bwght, breaks=30, main = 'Histogram with 30 bins',  xlab ='Birth Weigh
 hist(data$bwght, breaks=40, main = 'Histogram with 40 bins',  xlab ='Birth Weight')
 
 
+
+
 # QUESTION 4 --------------------------------------------------------------
 
 ## @knitr Question4-part1
@@ -62,3 +69,49 @@ hist(data$cigs, breaks=10, main = 'Histogram with 10 bins', xlab ='The Number of
 ## Histogram with 20 bins
 hist(data$cigs, breaks=20, main = 'Histogram with 20 bins',  xlab ='The Number of Smoked Cigarettes')
 
+
+
+
+# QUESTION 5 --------------------------------------------------------------
+
+## @knitr Question5-part1
+plot(data$cigs, data$bwght, xlab='Number of Smoked Cigarettes', ylab='Birth Weight', main='Scatterplot of Bwght Against Cigs')
+
+## @knitr Question5-part2
+bwght_cigs.lm = lm(bwght ~ cigs, data=data) 
+summary(bwght_cigs.lm)$r.squared
+
+
+
+
+# QUESTION 6 --------------------------------------------------------------
+ 
+## @knitr Question6-part1
+newdata = data[data[,"bwght"] != 0,]
+
+## @knitr Question6-part2
+bwght_cigs_2.lm = lm(bwght ~ cigs, data=newdata)
+summary(bwght_cigs_2.lm)$coefficients
+
+
+
+# QUESTION 7 --------------------------------------------------------------
+
+## @knitr Question7-part1
+summary(data$faminc)
+
+
+## @knitr Question7-part2
+quantile( data$faminc, probs = c(0.01, 0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95, 0.99) )
+
+## @knitr Question7-part3
+#### PLOT HISTOGRAM AND PDF OF Z
+par(mfrow = c(2, 2))
+## Histogram with 10 bins
+hist(data$faminc, breaks=10, main = 'Histogram with 10 bins', xlab ='Family Income in 1988')
+## Histogram with 20 bins
+hist(data$faminc, breaks=20, main = 'Histogram with 20 bins',  xlab ='Family Income in 1988')
+## Histogram with 30 bins
+hist(data$faminc, breaks=30, main = 'Histogram with 30 bins',  xlab ='Family Income in 1988')
+## Histogram with 40 bins
+hist(data$faminc, breaks=40, main = 'Histogram with 40 bins',  xlab ='Family Income in 1988')
