119a308a5a510283152abd44e6d832e974e55648
Juanjo Carin <juanjose.carin@ischool.berkeley.edu>
2016-01-31 16:01:18 -0800

    Question 4

--- HW1/code/W271_HW1_Carin.R
+++ HW1/code/W271_HW1_Carin.R
@@ -44,7 +44,8 @@ quantile(data$bwght, probs = c(1, 5, 10, 25, 50, 75, 90, 95, 99)/100)
 # Use hist and bin width = 5
 bin_width = 5
 hist(data$bwght, breaks = seq(floor(min(data$bwght)/bin_width)*bin_width, 
-                              ceiling(max(data$bwght)/bin_width)*bin_width, by = bin_width), 
+                              ceiling(max(data$bwght)/bin_width)*bin_width, 
+                              by = bin_width), 
      xlab = "Birth weight (ounces)", ylab = "Count", 
      main = "Histogram of birth weight")
 
@@ -62,7 +63,8 @@ ggplot(data = data, aes(bwght)) +
 #                  binwidth = bin_width) +  
 #   labs(x = "Birth weight (ounces)", y = "Density", 
 #        title = "Histogram of birth weight") + 
-#   stat_function(fun = dnorm, args = list(mean = mean(data$bwght, na.rm = TRUE), 
+#   stat_function(fun = dnorm, args = list(mean = mean(data$bwght, 
+#                                                      na.rm = TRUE), 
 #                                          sd = sd(data$bwght, na.rm = TRUE)), 
 #                 colour = 'black', size = 1)
 
@@ -70,7 +72,8 @@ ggplot(data = data, aes(bwght)) +
 # Use ggplot and bin width = 10
 bin_width = 10
 ggplot(data = data, aes(bwght)) + 
-  geom_histogram(binwidth = bin_width) +  
+  geom_histogram(colour = 'black', fill = 'white', 
+                 binwidth = bin_width) +  
   labs(x = "Birth weight (ounces)", y = "Count", 
        title = "Histogram of birth weight")
 
@@ -78,6 +81,57 @@ ggplot(data = data, aes(bwght)) +
 # Use ggplot and bin width = 20
 bin_width = 20
 ggplot(data = data, aes(bwght)) + 
-  geom_histogram(binwidth = bin_width) +  
+  geom_histogram(colour = 'black', fill = 'white', 
+                 binwidth = bin_width) +  
   labs(x = "Birth weight (ounces)", y = "Count", 
        title = "Histogram of birth weight")
+
+
+## @knitr Question4-1
+# QUESTION 4 --------------------------------------------------------------
+# Summarize the variable cigs: summary(data$cigs)
+summary(data$cigs)
+
+## @knitr Question4-2
+# List the following quantiles: 1%, 5%, 10%, 25%, 50%, 75%, 90%, 95%, 99%**
+quantile(data$cigs, probs = c(1, 5, 10, 25, 50, 75, 90, 95, 99)/100)
+
+## @knitr Question4-3-1
+# Plot the histogram of bwght and comment on the shape of its distribution
+# Use hist and bin width = 5
+bin_width = 1
+hist(data$cigs, breaks = seq(floor(min(data$cigs)/bin_width)*bin_width, 
+                             ceiling(max(data$cigs)/bin_width)*bin_width, 
+                             by = bin_width), 
+     xlab = "Cigarettes smoked each day by the mother while pregnant", 
+     ylab = "Count", 
+     main = "Histogram of cigarettes smoked each day\nby the mother while pregnant")
+
+## @knitr Question4-3-2
+# Use ggplot and bin width = 5
+ggplot(data = data, aes(cigs)) + 
+  geom_histogram(colour = 'black', fill = 'white', 
+                 binwidth = bin_width) +  
+  labs(x = "Cigarettes smoked each day by the mother while pregnant", 
+       y = "Count", 
+       title = "Histogram of cigarettes smoked each day\nby the mother while pregnant")
+
+## @knitr Question4-3-3
+# Use ggplot and bin width = 10
+bin_width = 5
+ggplot(data = data, aes(cigs)) + 
+  geom_histogram(colour = 'black', fill = 'white', 
+                 binwidth = bin_width) +  
+  labs(x = "Cigarettes smoked each day by the mother while pregnant", 
+       y = "Count", 
+       title = "Histogram of cigarettes smoked each day\nby the mother while pregnant")
+
+## @knitr Question4-3-4
+# Use ggplot and bin width = 20
+bin_width = 10
+ggplot(data = data, aes(cigs)) + 
+  geom_histogram(colour = 'black', fill = 'white', 
+                 binwidth = bin_width) +  
+  labs(x = "Cigarettes smoked each day by the mother while pregnant", 
+       y = "Count", 
+       title = "Histogram of cigarettes smoked each day\nby the mother while pregnant")
