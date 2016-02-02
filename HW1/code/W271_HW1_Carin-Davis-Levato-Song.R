0968919bbeadff28515922b58f9fe0ebf4b7b164
Juanjo Carin <juanjose.carin@ischool.berkeley.edu>
2016-02-02 13:22:58 -0800

    Final version (demo Q10)

--- HW1/code/W271_HW1_Carin-Davis-Levato-Song.R
+++ HW1/code/W271_HW1_Carin-Davis-Levato-Song.R
@@ -2,15 +2,52 @@
 ## Carin, Davis, Levato, Song ##
 
 
-## @knitr Libraries
-# LIBRARIES AND CONSTANTS -------------------------------------------------
+## @knitr Libraries-Functions-Constants
+# LIBRARIES, FUNCTIONS AND CONSTANTS -------------------------------------------------
 # Load libraries
-# Define constants used in multiple chunks
 library(ggplot2)
 library(knitr)
 library(car)
-# library(plot3D)
-# num_sim <- 100e3 # number of simulations
+# Define functions
+# A function to apply format (using formatC
+frmt <- function(qty, digits = 3) {
+  formatC(qty, digits = digits, format = "f", drop0trailing = FALSE, 
+                 big.mark = ",")
+}
+
+
+# A function that codes significance level
+sig_stars <- function(p) {
+  stars = symnum(p, na = F, cutpoints = c(0, .001, .01, .05, .1, 1), 
+                 symbols=c("**`***`**","**`** `**", "**`*  `**", "**.  **", 
+                           "   "))
+  return(stars)
+}
+# A function that draws a nice-looking table (following standard format for 
+# publication) with the summary of the regression model 
+create_regtable <- function(model, df, params, causes, effect) {
+  model_summary <- summary(model)
+  model_coefs <- model_summary$coefficients
+  estimate <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
+    paste0(frmt(model_coefs[x, 1]), sig_stars(model_coefs[x, 4]))))
+  SE <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
+    paste0("(", frmt(model_coefs[x, 2]), ")  ")))
+  N <- paste0(nrow(df), "   ")
+  R2 <- paste0(frmt(model_summary$r.squared), "   ")
+  Fstatistic <- paste0(frmt(model_summary$fstatistic[1]), "   ")
+  pvalue <- paste0(frmt(1 - pf(model_summary$fstatistic[1], 2, 300)), "   ")
+  table <- matrix(c(t(matrix(c(estimate, SE), ncol = 2)), R2, Fstatistic, 
+                    pvalue, N), ncol = 1)
+  rows <- NULL
+  for (cause in causes) {
+    rows <- c(rows, paste("**", cause, "**", sep = ""), "")
+  }
+  rownames(table) <- c(rows, "Baseline (Intercept)", " ", "$R^2$", "F", "p", 
+                       "N")
+  colnames(table) <- effect
+  return(table)
+}
+# Define constants used in multiple chunks
 
 
 
@@ -181,47 +218,7 @@ model <- lm(as.formula(paste("bwght", paste(params, sep = "",
                                             collapse = " + "), sep = " ~ ")), 
             data = data[data$bwght !=0, ])
 summary(model)
-
-# A function that codes significance level
-sig_stars = function(p) {
-  stars = symnum(p, na = F, cutpoints = c(0, .001, .01, .05, .1, 1), 
-                 symbols=c("**`***`**","**`** `**", "**`*  `**", "**.  **", 
-                           "   "))
-  return(stars)
-  }
-
-# A function that draws a nice-looking table (following standard format for 
-  # publication) with the summary of the regression model 
-create_regtable <- function(model, df, params, causes, effect) {
-  model_summary <- summary(model)
-  model_coefs <- model_summary$coefficients
-  estimate <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
-    paste0(formatC(model_coefs[x, 1], digits = 3, format = "f", 
-                   drop0trailing = FALSE), sig_stars(model_coefs[x, 4]))))
-  SE <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
-    paste0("(", formatC(model_coefs[x, 2], digits = 3, format = "f", 
-                        drop0trailing = FALSE), ")  ")))
-  N <- paste0(nrow(df), "   ")
-  R2 <- paste0(formatC(model_summary$r.squared, digits = 3, format = "f", 
-                       drop0trailing = FALSE), "   ")
-  Fstatistic <- paste0(formatC(model_summary$fstatistic[1], digits = 3, 
-                               format = "f", drop0trailing = FALSE), "   ")
-  pvalue <- paste0(formatC(1 - pf(model_summary$fstatistic[1], 2, 300), 
-                           digits = 3, format = "f", drop0trailing = FALSE), 
-                   "   ")
-  table <- matrix(c(t(matrix(c(estimate, SE), ncol = 2)), R2, Fstatistic, 
-                    pvalue, N), ncol = 1)
-  rows <- NULL
-  for (cause in causes) {
-    rows <- c(rows, paste("**", cause, "**", sep = ""), "")
-  }
-  rownames(table) <- c(rows, "Baseline (Intercept)", " ", "$R^2$", "F", "p", 
-                       "N")
-  colnames(table) <- effect
-  return(table)
-}
-
-# Create the table with the given parameters
+# Create the table with the given parameters (using function in 1st section)
 table <- create_regtable(model, data, params, 
                          c("Cigarettes smoked each day by the mother"), 
                          "Birth weight (ounces)")
@@ -303,3 +300,19 @@ kable(table, align = "r",
       caption = paste("Effect of the number of cigarettes smoked each day", 
                       "\nby the mother while pregnant and the family income\n", 
                       "on the birth weight", sep = ""))
+
+
+
+## @knitr Question10
+# QUESTION 10 --------------------------------------------------------------
+# Demonstration of the differences in the value of one coefficient
+# when another variable is included or omitted
+tilde_delta_1 <- summary(lm(faminc ~ cigs, 
+                            data = data[data$bwght != 0, ]))$coefficients[2, 1]
+hat_beta_1 <- summary(lm(bwght ~ cigs + faminc, 
+                         data = data[data$bwght != 0, ]))$coefficients[2, 1]
+hat_beta_2 <- summary(lm(bwght ~ cigs + faminc, 
+                         data = data[data$bwght != 0, ]))$coefficients[3, 1]
+hat_beta_1 + hat_beta_2 * tilde_delta_1
+(tilde_beta_1 <- summary(lm(bwght ~ cigs, 
+                            data = data[data$bwght != 0, ]))$coefficients[2, 1])
