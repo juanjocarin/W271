92971c73e63909e2d1d4d23236ad5e99ea0b7a02
Juanjo Carin <juanjose.carin@ischool.berkeley.edu>
2016-02-07 14:54:27 -0800

    Q7, better plots, RSEs (only pending: details of Q5 and Q6)

--- HW2/code/W271_HW2_template.R
+++ HW2/code/W271_HW2_template.R
@@ -6,70 +6,112 @@
 # LIBRARIES, FUNCTIONS AND CONSTANTS -------------------------------------------------
 # Load libraries
 library(ggplot2)
+library(ggfortify)
 library(knitr)
 library(pastecs)
 
 # Define functions
-    # THE FOLLOWING FUNCTIONS ARE JUST FOR FORMATTING PURPOSES
-    # A function to apply format (using formatC
-    frmt <- function(qty, digits = 3) {
-      formatC(qty, digits = digits, format = "f", drop0trailing = FALSE, 
-                     big.mark = ",")
-    }
-    # A function that codes significance level
-    sig_stars <- function(p) {
-      stars = symnum(p, na = F, cutpoints = c(0, .001, .01, .05, .1, 1), 
-                     symbols=c("**`***`**","**`** `**", "**`*  `**", "**.  **", 
-                               "   "))
-      return(stars)
-    }
-    # A function that draws a nice-looking table (following standard format for 
-    # publication) with the summary of the regression model 
-    create_regtable <- function(model, params, causes, effect) {
-      model_summary <- summary(model)
-      model_coefs <- model_summary$coefficients
-      estimate <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
-        paste0(frmt(model_coefs[x, 1]), sig_stars(model_coefs[x, 4]))))
-      SE <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
-        paste0("(", frmt(model_coefs[x, 2]), ")  ")))
-      N <- paste0(length(model_summary$residuals), "   ")
-      R2 <- paste0(frmt(model_summary$r.squared), "   ")
-      Fsttstc <- model_summary$fstatistic
-      Fstatistic <- paste0(frmt(Fsttstc["value"]), "   ")
-      pvalue <- paste0(frmt(1 - pf(q = Fsttstc["value"], 
-                                   df1 = Fsttstc["numdf"], 
-                                   df2 = Fsttstc["dendf"])), "   ")
-      table <- matrix(c(t(matrix(c(estimate, SE), ncol = 2)), R2, Fstatistic, 
-                        pvalue, N), ncol = 1)
-      rows <- NULL
-      for (cause in causes) {
-        rows <- c(rows, paste("**", cause, "**", sep = ""), "")
-      }
-      rownames(table) <- c(rows, "Baseline (Intercept)", " ", "$R^2$", "F", "p", 
-                           "N")
-      colnames(table) <- effect
-      return(table)
-    }
-    # Functions to count & refer figures & tables
-    # http://rmflight.github.io/posts/2012/10/papersinRmd.html
-    incCount <- function(inObj, useName) {
-      nObj <- length(inObj)
-      useNum <- max(inObj) + 1
-      inObj <- c(inObj, useNum)
-      names(inObj)[nObj + 1] <- useName
-      inObj
-    }
-    pasteLabel <- function(preText, inObj, objName, insLink = TRUE) {
-      objNum <- inObj[objName]
-      
-      useText <- paste(preText, objNum, sep = " ")
-      if (insLink) {
-        useText <- paste("[", useText, "](#", objName, ")", sep = "")
-      }
-      useText
-    }
-    figCount <- c(`_` = 0)
-    tableCount <- c(`_` = 0)
+# A function to calculate Robust Standard Errors
+RSEs <- function(model){
+  require(sandwich, quietly = TRUE)
+  require(lmtest, quietly = TRUE)
+  newSE <- vcovHC(model)
+  coeftest(model, newSE)
+}
+
+# THE FOLLOWING FUNCTIONS ARE JUST FOR FORMATTING PURPOSES
+
+# A function to apply format (using formatC
+frmt <- function(qty, digits = 3) {
+  formatC(qty, digits = digits, format = "f", drop0trailing = FALSE, 
+          big.mark = ",")
+}
+
+# A function that codes significance level
+sig_stars <- function(p) {
+  stars = symnum(p, na = F, cutpoints = c(0, .001, .01, .05, .1, 1), 
+                 symbols=c("**`***`**","**`** `**", "**`*  `**", "**.  **", 
+                           "   "))
+  return(stars)
+}
+
+# A function that draws a nice-looking table (following standard format for 
+# publication) with the summary of the regression model 
+create_regtable <- function(model, params, causes, effect) {
+  model_summary <- summary(model)
+  model_coefs <- model_summary$coefficients
+  estimate <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
+    paste0(frmt(model_coefs[x, 1]), sig_stars(model_coefs[x, 4]))))
+  SE <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
+    paste0("(", frmt(model_coefs[x, 2]), ")  ")))
+  N <- paste0(length(model_summary$residuals), "   ")
+  R2 <- paste0(frmt(model_summary$r.squared), "   ")
+  Fsttstc <- model_summary$fstatistic
+  Fstatistic <- paste0(frmt(Fsttstc["value"]), "   ")
+  pvalue <- paste0(frmt(1 - pf(q = Fsttstc["value"], 
+                               df1 = Fsttstc["numdf"], 
+                               df2 = Fsttstc["dendf"])), "   ")
+  table <- matrix(c(t(matrix(c(estimate, SE), ncol = 2)), R2, Fstatistic, 
+                    pvalue, N), ncol = 1)
+  rows <- NULL
+  for (cause in causes) {
+    rows <- c(rows, paste("**", cause, "**", sep = ""), "")
+  }
+  rownames(table) <- c(rows, "Baseline (Intercept)", " ", "$R^2$", "F", "p", 
+                       "N")
+  colnames(table) <- effect
+  return(table)
+}
+
+# Same function to draw table with regression results, but using robust SEs
+create_regtable_RSEs <- function(model, params, causes, effect) {
+  model_summary <- summary(model)
+  model_coefs <- RSEs(model)
+  estimate <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
+    paste0(frmt(model_coefs[x, 1]), sig_stars(model_coefs[x, 4]))))
+  SE <- unlist(lapply(c(seq(2, 1+length(params)), 1), function(x) 
+    paste0("(", frmt(model_coefs[x, 2]), ")  ")))
+  N <- paste0(length(model_summary$residuals), "   ")
+  R2 <- paste0(frmt(model_summary$r.squared), "   ")
+  Fsttstc <- model_summary$fstatistic
+  Fstatistic <- paste0(frmt(Fsttstc["value"]), "   ")
+  pvalue <- paste0(frmt(1 - pf(q = Fsttstc["value"], 
+                               df1 = Fsttstc["numdf"], 
+                               df2 = Fsttstc["dendf"])), "   ")
+  table <- matrix(c(t(matrix(c(estimate, SE), ncol = 2)), R2, Fstatistic, 
+                    pvalue, N), ncol = 1)
+  rows <- NULL
+  for (cause in causes) {
+    rows <- c(rows, paste("**", cause, "**", sep = ""), "")
+  }
+  rownames(table) <- c(rows, "Baseline (Intercept)", " ", "$R^2$", "F", "p", 
+                       "N")
+  colnames(table) <- effect
+  return(table)
+}
+
+# Functions to count & refer figures & tables
+# http://rmflight.github.io/posts/2012/10/papersinRmd.html
+incCount <- function(inObj, useName) {
+  nObj <- length(inObj)
+  useNum <- max(inObj) + 1
+  inObj <- c(inObj, useNum)
+  names(inObj)[nObj + 1] <- useName
+  inObj
+}
+pasteLabel <- function(preText, inObj, objName, insLink = TRUE) {
+  objNum <- inObj[objName]
+  
+  useText <- paste(preText, objNum, sep = " ")
+  if (insLink) {
+    useText <- paste("[", useText, "](#", objName, ")", sep = "")
+  }
+  useText
+}
+# Counters for Figures and Tables
+figCount <- c(`_` = 0)
+tableCount <- c(`_` = 0)
+
 # Define constants
 
 
