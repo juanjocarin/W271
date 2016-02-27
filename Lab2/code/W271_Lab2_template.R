16aec92cf66610fea530314f0361e132325d16c8
Juanjo Carin <juanjose.carin@ischool.berkeley.edu>
2016-02-26 18:45:54 -0800

    Lab2 template

---
+++ Lab2/code/W271_Lab2_template.R
@@ -0,0 +1,171 @@
+## MIDS W271-4 Lab2           ##
+## Carin, Davis, Levato, Song ##
+
+
+## @knitr Libraries-Functions-Constants
+# LIBRARIES, FUNCTIONS AND CONSTANTS -------------------------------------------------
+# Load libraries
+library(e1071)
+library(ggplot2)
+library(ggfortify)
+library(scales)
+library(knitr)
+library(pastecs)
+library(car)
+library(sandwich)
+library(lmtest)
+library(dplyr)
+library(stargazer)
+library(texreg)
+
+# Define functions
+
+# THE FOLLOWING FUNCTIONS ARE JUST FOR FORMATTING PURPOSES
+
+# A function to apply format
+frmt <- function(qty, digits = 3) {
+  formatC(qty, digits = digits, format = "f", drop0trailing = FALSE, 
+          big.mark = ",")
+}
+
+# A function that codes significance level
+sig_stars <- function(p) {
+  stars = symnum(p, na = F, cutpoints = c(0, .001, .01, .05, .1, 1), 
+                 symbols=c("$^{***}$","$^{**}$", "$^{*}$", 
+                           "$^{\\mathbf{\\cdot}}$", ""))
+  return(stars)
+}
+
+# A function that draws a nice-looking table, based on stargazer
+# USING HETEROSKEDASTICTY-ROBUST STANDARD ERRORS AND F STATISTIC
+stargazer2 <- function(models, type = 'latex', ...) {
+  if (class(models) != "lm") {
+    model_list <- models
+  } else {
+    model_list <- list()
+    model_list[[1]] <- models
+  }
+  stargazer(model_list, type = type, header = FALSE, table.placement = "h!", 
+            star.cutoffs = c(0.1, 0.05, 0.01, 0.001), 
+            star.char = c("\\mathbf{\\cdot}", "*", "**", "***"), 
+            notes.append = FALSE, notes.label = "", 
+            notes = "$\\cdot$p<0.1; *p<0.05; **p<0.01; ***p<0.001", 
+            se = lapply(model_list, function(m) coeftest(m, vcovHC(m))[, 2]), 
+            p = lapply(model_list, function(m) coeftest(m, vcovHC(m))[, 4]), 
+            add.lines = 
+              list(c("F Statistic", unlist(lapply(model_list, function(m) 
+                paste0(frmt(waldtest(m, vcov=vcovHC)[2,3]), 
+                       sig_stars(waldtest(m, vcov=vcovHC)$`Pr(>F)`[2]))))), 
+                c("df", unlist(lapply(model_list, function(m) 
+                  paste0(abs(waldtest(m, vcov=vcovHC)$Df[2]), "; ", 
+                         waldtest(m, vcov=vcovHC)$Res.Df[1]))))), 
+            df = FALSE, no.space = TRUE, omit.stat = "f", ...)
+}
+
+# Another (2) function(s) to draw tables, based on stargazer
+# also USING HETEROSKEDASTICTY-ROBUST STANDARD ERRORS AND F STATISTIC
+createTexreg2 <- function(model, ...) {
+  model_summary <- summary(model)
+  coefs <- coeftest(model, vcovHC(model))
+  f <- waldtest(model, vcov=vcovHC)
+  createTexreg(coef = coefs[, 1], coef.names = rownames(coefs), 
+               se = coefs[, 2], pvalues = coefs[, 4], 
+               gof = c(model_summary$r.squared, model_summary$adj.r.squared, 
+                       f$F[2], length(model$residuals)), 
+               gof.names = c("R$^2$", "R$^2_{\\text{adj}}$", "F", "N"), 
+               gof.decimal = c(rep(TRUE, 3), FALSE), ...)
+}
+
+texreg2 <- function(list, ...) {
+  texreg(list, digits = 3, caption.above = TRUE, bold = 0.05, float.pos = 'h!', 
+         stars = c(0.001, 0.01, 0.05, 0.1), symbol = "\\cdot", ...)
+}
+
+# Functions to count & refer figures & tables
+# http://rmflight.github.io/posts/2012/10/papersinRmd.html
+incCount <- function(inObj, useName) {
+  nObj <- length(inObj)
+  useNum <- max(inObj) + 1
+  inObj <- c(inObj, useNum)
+  names(inObj)[nObj + 1] <- useName
+  inObj
+}
+pasteLabel <- function(preText, inObj, objName, insLink = TRUE) {
+  objNum <- inObj[objName]
+  
+  useText <- paste(preText, objNum, sep = " ")
+  if (insLink) {
+    useText <- paste("[", useText, "](#", objName, ")", sep = "")
+  }
+  useText
+}
+# Counters for Figures and Tables
+figCount <- c(`_` = 0)
+tableCount <- c(`_` = 0)
+
+# Define constants
+
+
+
+## @knitr Question1-1
+# QUESTION 1 --------------------------------------------------------------
+# Install the library "astsa" using the function: install.packages("astsa")
+# Check if already installed; if not, install it
+if (!"astsa" %in% installed.packages()[, "Package"]) install.packages("astsa")
+
+## @knitr Question1-2
+# Load the library: library(astsa)
+library(astsa)
+# Last two commands can be substituted by simply...
+if (!require(astsa)) install.packages("astsa")
+
+## @knitr Question1-3
+# Use the function str() to see the information of a particular data series, 
+# such as str(EQ5) for the Seismic Trace of Earthquake number 5 series
+str(EQ5)
+str(flu)
+str(gas)
+
+## @knitr Question1-4
+# Plot the time series plots and histograms of the following 3 series.
+
+## @knitr Question1-5
+# Write a few sentences to describe each of the series: EQ5, flu, and gas
+
+
+
+## @knitr Question2
+# QUESTION 2 --------------------------------------------------------------
+# Describe 3 examples you have used in your work or encounter in real life. 
+# Ideally, you can even load at least one of these time series, plot it, 
+# and the write a few statements to describe its characteristics.
+
+
+
+## @knitr Question3
+# QUESTION 3 --------------------------------------------------------------
+# Simulate a white noise series with 1000 random draws and plot 
+# (1) a time series plot and
+# (2) a histogram.
+
+
+
+## @knitr Question4
+# QUESTION 4 --------------------------------------------------------------
+# Simulate (with 1000 random draws) the following two zero-mean 
+# autoregressive model with order 1 (i.e. AR(1)) models:
+# yt = 0.9yt1 + w
+# yt = 0.2yt1 + w
+# Plot a time plot for each of the simulated series. 
+# Graph a histogram for each of the simulated series.
+
+
+
+## @knitr Question5
+# QUESTION 5 --------------------------------------------------------------
+# Simulate (with 1000 random draws) the following 3 models:
+# 1. A deterministic linear (time) trend of the form: yt = 10 + 0.5t
+# 2. Random walk without drift
+# 3. Random walk with drift = 0.5
+# Plot a time plot for each of the simulated series. 
+# Graph a histogram for each of the simulated series.
