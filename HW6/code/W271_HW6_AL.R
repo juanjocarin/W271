91706190f129ffdb02524012367b17bbd4abb1f7
ashley-dsci <ashley-dsci@ischool.berkeley.edu>
2016-03-10 16:23:03 -0500

    Q1

---
+++ HW6/code/W271_HW6_AL.R
@@ -0,0 +1,49 @@
+## MIDS W271-4 HW6            ##
+## Carin, Davis, Levato, Song ##
+
+
+## @knitr Libraries-Functions-Constants
+# LIBRARIES, FUNCTIONS AND CONSTANTS -------------------------------------------------
+# Load libraries
+# library(e1071)
+library(ggplot2)
+library(ggfortify)
+library(scales)
+library(knitr)
+# library(pastecs)
+# library(car)
+# library(sandwich)
+# library(lmtest)
+# library(dplyr)
+# library(stargazer)
+# library(texreg)
+#library(weatherData)
+library(scales)
+library(xts)
+library(reshape2)
+
+# Define functions
+
+# THE FOLLOWING FUNCTIONS ARE JUST FOR FORMATTING PURPOSES
+
+# A function to apply format
+frmt <- function(qty, digits = 3) {
+  formatC(qty, digits = digits, format = "f", drop0trailing = FALSE, 
+          big.mark = ",")
+}
+
+# Define constants
+
+
+
+## @knitr Question1-1
+# QUESTION 1 --------------------------------------------------------------
+
+
+
+
+## @knitr Question2-1
+# QUESTION 2 --------------------------------------------------------------
+
+
+## @knitr Question2-2
