bbfb9f31ea6efb94567e833e6f88d3fc1f91f3fb
songminghu2004 <songminghu2004@yahoo.com>
2016-03-13 19:41:43 -0400

    HW6 Q4 a-f1
    HW6 Q4 a-f1

--- HW6/code/W271_HW6_MHSONG.R
+++ HW6/code/W271_HW6_MHSONG.R
@@ -123,3 +123,17 @@ autoplot(pacf(INJCJC_ts, plot = FALSE), xlab = "Lag",main="Partial Autocorrelati
 
 ## @knitr Question4-5-5
 gglagplot(INJCJC_ts, lags = 9, nrow = 3, ncol = 3)
+
+## @knitr Question4-6-1
+INJCJC_ts_rep5 = filter(INJCJC_ts, sides=2, rep(1,5)/5)
+INJCJC_ts_rep52 = filter(INJCJC_ts, sides=2, rep(1,52)/52)
+plot(INJCJC_ts, main="Moving Average Smoothing for Initial Unemployment Claims", 
+     pch=4, lty=5, lwd=1, xlab="Year", 
+     ylab="Number of Filed Jobless Claims")
+lines(INJCJC_ts_rep5, lty=1, lwd=1.5, col="green")
+lines(INJCJC_ts_rep52, lty=1, lwd=1.5, col="blue")
+# Add Legend
+leg.txt <- c("Original Series", "5-Point Symmetric Moving Average", "52-Point Symmetric Moving Average")
+legend("topleft", legend=leg.txt, lty=c(1,1,1), col=c("black","green","blue"),
+       bty='n', cex=1, merge = TRUE, bg=336)
+
