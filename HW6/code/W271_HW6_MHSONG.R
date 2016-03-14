d57d32aa3852780acc5c2ce646bae4b8cf926e04
songminghu2004 <songminghu2004@yahoo.com>
2016-03-13 21:11:48 -0400

    HW6 Q4 f
    HW6 Q4 f

--- HW6/code/W271_HW6_MHSONG.R
+++ HW6/code/W271_HW6_MHSONG.R
@@ -127,7 +127,7 @@ gglagplot(INJCJC_ts, lags = 9, nrow = 3, ncol = 3)
 ## @knitr Question4-6-1
 INJCJC_ts_rep5 = filter(INJCJC_ts, sides=2, rep(1,5)/5)
 INJCJC_ts_rep52 = filter(INJCJC_ts, sides=2, rep(1,52)/52)
-plot(INJCJC_ts, main="Moving Average Smoothing for Initial Unemployment Claims", 
+plot(INJCJC_ts, main="Moving Average Smoothing for INJCJC_ts", 
      pch=4, lty=5, lwd=1, xlab="Year", 
      ylab="Number of Filed Jobless Claims")
 lines(INJCJC_ts_rep5, lty=1, lwd=1.5, col="green")
@@ -135,5 +135,67 @@ lines(INJCJC_ts_rep52, lty=1, lwd=1.5, col="blue")
 # Add Legend
 leg.txt <- c("Original Series", "5-Point Symmetric Moving Average", "52-Point Symmetric Moving Average")
 legend("topleft", legend=leg.txt, lty=c(1,1,1), col=c("black","green","blue"),
-       bty='n', cex=1, merge = TRUE, bg=336)
+       bty='n', cex=0.8, merge = TRUE, bg=336)
 
+## @knitr Question4-6-2
+wk = time(INJCJC_ts) - mean(time(INJCJC_ts))  
+wk2 = wk^2 
+wk3 = wk^3
+cs = cos(2*pi*wk)  
+sn = sin(2*pi*wk)
+reg1 = lm(INJCJC_ts~wk + wk2 + wk3, na.action=NULL)
+reg2 = lm(INJCJC_ts~wk + wk2 + wk3 + cs + sn, na.action=NULL)
+plot(INJCJC_ts, main="Regression & Periodic Smoothing for INJCJC_ts", 
+     pch=4, lty=5, lwd=1, xlab="Year", 
+     ylab="Number of Filed Jobless Claims")
+lines(fitted(reg1), lty=1, lwd=1.5, col="green")
+lines(fitted(reg2), lty=1, lwd=1.5, col="blue")
+# Add Legend
+leg.txt <- c("Original Series", "Cubic Trend Regression Smoothing", "Periodic Regression Smoothing")
+legend("topleft", legend=leg.txt, lty=c(1,1,1), col=c("black","green","blue"),
+       bty='n', cex=0.8, merge = TRUE, bg=336)
+
+
+## @knitr Question4-6-3
+plot(INJCJC_ts, main="Kernel Smoothing for INJCJC_ts", 
+     pch=4, lty=5, lwd=1, xlab="Year", 
+     ylab="Number of Filed Jobless Claims")
+lines(ksmooth(time(INJCJC_ts), INJCJC_ts, "normal", bandwidth=5/52),lty=1, lwd=1.5, col="green")
+lines(ksmooth(time(INJCJC_ts), INJCJC_ts, "normal", bandwidth=0.5),lty=1, lwd=1.5, col="blue")
+# Add Legend
+leg.txt <- c("Original Series", "Kernel Smoothing: bandwidth=5/52", "Kernel Smoothing: bandwidth=0.5")
+legend("topleft", legend=leg.txt, lty=c(1,1,1), col=c("black","green","blue"),
+       bty='n', cex=0.8, merge = TRUE, bg=336)
+
+## @knitr Question4-6-4
+plot(INJCJC_ts, main="Nearest Neighborhood Smoothing for INJCJC_ts", 
+     pch=4, lty=5, lwd=1, xlab="Year", 
+     ylab="Number of Filed Jobless Claims")
+lines(supsmu(time(INJCJC_ts), INJCJC_ts, span=.01),lty=1, lwd=1.5, col="green")
+lines(supsmu(time(INJCJC_ts), INJCJC_ts, span=.05),lty=1, lwd=1.5, col="blue")
+# Add Legend
+leg.txt <- c("Original Series", "NN Smoothing: bandwidth=.01", "NN Smoothing: bandwidth=.05")
+legend("topleft", legend=leg.txt, lty=c(1,1,1), col=c("black","green","blue"),
+       bty='n', cex=0.8, merge = TRUE, bg=336)
+
+## @knitr Question4-6-5
+plot(INJCJC_ts, main="LOWESS Smoothing for INJCJC_ts", 
+     pch=4, lty=5, lwd=1, xlab="Year", 
+     ylab="Number of Filed Jobless Claims")
+lines(lowess(INJCJC_ts, f=.001),lty=1, lwd=1.5, col="green")
+lines(lowess(INJCJC_ts, f=0.025),lty=1, lwd=1.5, col="blue")
+# Add Legend
+leg.txt <- c("Original Series", "LOWESS Smoothing: bandwidth=.001", "LOWESS Smoothing: bandwidth=0.025")
+legend("topleft", legend=leg.txt, lty=c(1,1,1), col=c("black","green","blue"),
+       bty='n', cex=0.8, merge = TRUE, bg=336)
+
+## @knitr Question4-6-6
+plot(INJCJC_ts, main="Spline Smoothing for INJCJC_ts", 
+     pch=4, lty=5, lwd=1, xlab="Year", 
+     ylab="Number of Filed Jobless Claims")
+lines(smooth.spline(time(INJCJC_ts), INJCJC_ts, spar=0.05),lty=1, lwd=1.5, col="green")          
+lines(smooth.spline(time(INJCJC_ts), INJCJC_ts, spar=0.7),lty=1, lwd=1.5, col="blue")  
+# Add Legend
+leg.txt <- c("Original Series", "Spline: Smoothing Parameter=.05", "Spline: Smoothing Parameter=0.7")
+legend("topleft", legend=leg.txt, lty=c(1,1,1), col=c("black","green","blue"),
+       bty='n', cex=0.8, merge = TRUE, bg=336)
